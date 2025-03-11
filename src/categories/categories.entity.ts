177a8e1496c804b5f1528a43bd7ab9666019c4d2
PhamVanTinh273 <tinh101334@donga.edu.vn>
2025-03-11 11:54:18 +0700

    update

--- src/categories/categories.entity.ts
+++ src/categories/categories.entity.ts
@@ -1,10 +1,14 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { ObjectType, Field } from '@nestjs/graphql';
 
+@ObjectType()  // Decorator để chỉ định đây là một loại GraphQL
 @Entity()
 export class Category {
+  @Field()  // Thêm decorator Field cho GraphQL
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Field()  // Thêm decorator Field cho GraphQL
   @Column()
   name: string;
 }
--- src/products/products.entity.ts
+++ src/products/products.entity.ts
@@ -1,19 +1,26 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { ObjectType, Field } from '@nestjs/graphql';
 
+@ObjectType()  // Decorator để chỉ định đây là một loại GraphQL
 @Entity()
 export class Product {
+  @Field()  // Thêm decorator Field cho GraphQL
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Field()  // Thêm decorator Field cho GraphQL
   @Column()
   name: string;
 
+  @Field()  // Thêm decorator Field cho GraphQL
   @Column('float')
   price: number;
 
+  @Field()  // Thêm decorator Field cho GraphQL
   @Column()
   description: string;
 
+  @Field()  // Thêm decorator Field cho GraphQL
   @Column()
   categoryId: string;
 }
